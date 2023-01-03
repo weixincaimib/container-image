67c589fb97411c685431dc609d7e1de3e7a3ad7a
One Person’s Revelry <69625113+aluopy@users.noreply.github.com>
2023-01-03 16:40:52 +0800

    Create docker-entrypoint.sh

---
+++ lxfpsc/docker-entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+KPFW_URL_FILE=/apps/lxfpsc/url.cnf
+
+KPFW_URL="${KPFW_URL:-127.0.0.1}"
+KPFW_PORT="${KPFW_PORT:-8082}"
+
+[[ -f ${KPFW_URL_FILE} ]] && rm -rf ${KPFW_URL_FILE}
+echo "${KPFW_URL}:${KPFW_PORT}" > ${KPFW_URL_FILE}
+
+exec "$@"
