889bd43ac93f7e187f7e5f1f3a9045de4339aa83
One Person’s Revelry <69625113+aluopy@users.noreply.github.com>
2023-01-04 17:25:05 +0800

    Update docker-entrypoint.sh

--- lxfpsc/docker-entrypoint.sh
+++ lxfpsc/docker-entrypoint.sh
@@ -5,7 +5,7 @@ set -o nounset
 set -o pipefail
 
 KPFW_URL_FILE=/app/lxfpsc/url.cnf
-KPFW_IP="${KPFW_URL:-127.0.0.1}"
+KPFW_IP="${KPFW_IP:-127.0.0.1}"
 KPFW_PORT="${KPFW_PORT:-8082}"
 
 [[ -f ${KPFW_URL_FILE} ]] && rm -rf ${KPFW_URL_FILE}
